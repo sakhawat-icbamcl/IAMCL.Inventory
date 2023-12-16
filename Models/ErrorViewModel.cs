feb7aa5bdc12edff5c762a708b45d19238bbcc37
User <User@DESKTOP-GDP1CET>
2023-12-17 04:50:09 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IAMCL.Inventory.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
